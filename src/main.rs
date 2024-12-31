f5087a1f923e59803fc18e59797bdc30f9dbfc82
Crusader13 <39165985+Crusader13@users.noreply.github.com>
2024-12-31 19:57:27 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use serenity::{
+    async_trait,
+    client::Client,
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+};
+
+const HELP_MESSAGE: &str = "Hallo Hilfe! Hau rein <#1323720623243001906> channel.";
+
+const HELP_COMMAND: &str = "!help";
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, context: Context, message: Message) {
+        if message.content == HELP_COMMAND {
+            if let Err(error) = message.channel_id.say(&context.http, HELP_MESSAGE).await {
+                eprintln!("Error sending message: {:?}", error);
+            }
+        }
+    }
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} ist gestartet!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token =
+        env::var("DISCORD_TOKEN").expect("Die DISCORD_TOKEN Umgebungsvariable gibt es nicht");
+
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Der Client konnte nicht erstellt werden!");
+
+    if let Err(err) = client.start().await {
+        eprintln!("Client fehler: {:?}", err);
+    }
 }
