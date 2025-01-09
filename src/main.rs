c530445c6359476826da6fd7eb9bdeac9b4c5789
Crusader13 <39165985+Crusader13@users.noreply.github.com>
2025-01-09 16:00:55 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,20 @@
 use poise::serenity_prelude as serenity;
-use serenity::{
-    prelude::*,
-    async_trait,
-    client::Client,
-    model::{channel::Message, gateway::Ready},
-};
+use serenity::prelude::*;
 use std::env;
 
 mod server;
 
-struct Handler;
+struct Data {}
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
 
-#[async_trait]
-impl EventHandler for Handler {
-    async fn message(&self, context: Context, message: Message) {
-        if message.content == HELP_COMMAND {
-            if let Err(error) = message.channel_id.say(&context.http, HELP_MESSAGE).await {
-                eprintln!("Error sending message: {:?}", error);
-            }
-        }
-    }
-    async fn ready(&self, _: Context, ready: Ready) {
-        println!("{} ist gestartet!", ready.user.name);
-    }
+#[poise::command(
+    slash_command,
+)]
+async fn test(ctx: Context<'_>) -> Result<(), Error> {
+    let antwort = "Hallo du Sau!";
+    ctx.say(antwort).await?;
+    Ok(())
 }
 
 #[tokio::main]
@@ -33,12 +25,21 @@ async fn main() {
     let intents =
         GatewayIntents::GUILD_MESSAGES | GatewayIntents::GUILDS | GatewayIntents::MESSAGE_CONTENT;
 
-    let mut client = Client::builder(&token, intents)
-        .event_handler(Handler)
-        .await
-        .expect("Der Client konnte nicht erstellt werden!");
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![test()],
+            ..Default::default()
+        })
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        })
+        .build();
 
-    if let Err(err) = client.start().await {
-        eprintln!("Client fehler: {:?}", err);
-    }
+    let client = serenity::ClientBuilder::new(token, intents)
+        .framework(framework)
+        .await;
+    client.unwrap().start().await.unwrap();
 }
